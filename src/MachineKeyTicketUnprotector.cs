f07de9fbaf4e7a3e5864a655fe4bcd8979928930
Cooperliu <Cooper_liu@outlook.com>
2020-03-05 15:19:22 +0800

    fix null reference issues

--- src/MachineKeyTicketUnprotector.cs
+++ src/MachineKeyTicketUnprotector.cs
@@ -15,16 +15,20 @@ namespace AspNetTicketBridge
         /// <param name="decryptionAlgorithm">The machineKey decryptionAlgorithm found in your web.config (Auto == AES)</param>
         /// <param name="validationAlgorithm">The machineKey validationAlgorithm found in your web.config</param>
         /// <returns>A v3 AuthenticationTicket</returns>
-        public static OwinAuthenticationTicket UnprotectOAuthToken(string token, string decryptionKey, string validationKey, 
+        public static OwinAuthenticationTicket UnprotectOAuthToken(string token, string decryptionKey, string validationKey,
             string decryptionAlgorithm = "AES", string validationAlgorithm = "HMACSHA1")
         {
             var decoded = WebEncoders.Base64UrlDecode(token);
 
-            var unprotected = Unprotect(decoded, decryptionKey, validationKey, 
+            var unprotected = Unprotect(decoded, decryptionKey, validationKey,
                 decryptionAlgorithm, validationAlgorithm,
                 "User.MachineKey.Protect",
                 "Microsoft.Owin.Security.OAuth", "Access_Token", "v1");
 
+            if (unprotected == null)
+            {
+                return null;
+            }
             var serializer = new OwinTicketSerializer();
             var ticket = serializer.Deserialize(unprotected);
             return ticket;
@@ -49,7 +53,10 @@ namespace AspNetTicketBridge
                 decryptionAlgorithm, validationAlgorithm,
                 "User.MachineKey.Protect",
                 "Microsoft.Owin.Security.OAuth", "Refresh_Token", "v1");
-
+            if (unprotected == null)
+            {
+                return null;
+            }
             var serializer = new OwinTicketSerializer();
             var ticket = serializer.Deserialize(unprotected);
             return ticket;
@@ -65,16 +72,19 @@ namespace AspNetTicketBridge
         /// <param name="decryptionAlgorithm">The machineKey decryptionAlgorithm found in your web.config (Auto == AES)</param>
         /// <param name="validationAlgorithm">The machineKey validationAlgorithm found in your web.config</param>
         /// <returns>A v3 AuthenticationTicket</returns>
-        public static OwinAuthenticationTicket UnprotectCookie(string cookie, string decryptionKey, string validationKey, 
+        public static OwinAuthenticationTicket UnprotectCookie(string cookie, string decryptionKey, string validationKey,
             string decryptionAlgorithm = "AES", string validationAlgorithm = "HMACSHA1")
         {
             var decoded = WebEncoders.Base64UrlDecode(cookie);
 
             var unprotected = Unprotect(decoded, decryptionKey, validationKey,
                 decryptionAlgorithm, validationAlgorithm,
-                "User.MachineKey.Protect", 
+                "User.MachineKey.Protect",
                 "Microsoft.Owin.Security.Cookies.CookieAuthenticationMiddleware", "ApplicationCookie", "v1");
-
+            if (unprotected == null)
+            {
+                return null;
+            }
             var serializer = new OwinTicketSerializer();
             var ticket = serializer.Deserialize(unprotected);
             return ticket;
@@ -85,7 +95,7 @@ namespace AspNetTicketBridge
             string primaryPurpose, params string[] purposes)
         {
             var unprotectedData = MachineKey.Unprotect(decodedData, validationKey, decryptionKey,
-                decryptionAlgorithm, validationAlgorithm, 
+                decryptionAlgorithm, validationAlgorithm,
                 primaryPurpose, purposes);
             return unprotectedData;
         }
